76f15f3836375e3ea8899b9bb8db9f829739b13a
kartikey2004-git <kartikeybhatnagar247@gmail.com>
2025-03-28 23:49:55 +0530

    user authentication , setup neonDB , and landing page UI

--- app/page.js
+++ app/page.js
@@ -1,8 +1,142 @@
+import TestimonialCarousel from "@/components/testimonial";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowRight, Calendar, Clock, LinkIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+
+const features = [
+  {
+    icon: Calendar,
+    title: "Create Events",
+    description: "Easily set up and customize your event types",
+  },
+  {
+    icon: Clock,
+    title: "Manage Availability",
+    description: "Define your availability to streamline scheduling",
+  },
+  {
+    icon: LinkIcon,
+    title: "Custom Links",
+    description: "Share your personalized scheduling link",
+  },
+];
+
+const howItWorks = [
+  { step: "Sign Up", description: "Create your free Schedulrr account" },
+  {
+    step: "Set Availability",
+    description: "Define when you're available for meetings",
+  },
+  {
+    step: "Share Your Link",
+    description: "Send your scheduling link to clients or colleagues",
+  },
+  {
+    step: "Get Booked",
+    description: "Receive confirmations for new appointments automatically",
+  },
+];
 
 export default function Home() {
   return (
-    <Button>hello</Button>
+    <main className="container mx-auto px-4 py-16">
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-12 mb-24">
+        <div className="lg:w-1/2">
+          <h1 className="text-7xl font-extrabold pb-6 gradient-title">
+            Streamline Your Scheduling
+          </h1>
+          <p className="text-xl text-gray-600 mb-10">
+            Schedulerrr transforms the way you manage time. Effortlessly
+            schedule events, define your availability, and allow others to book
+            time with you—smoothly and without any trouble
+          </p>
+          <Link href={"/dashoard"}>
+            <Button size={"lg"} className={"text-lg"}>
+              Get Started <ArrowRight className="ml-2 h-5 w-5" />
+            </Button>
+          </Link>
+        </div>
+
+        <div className="lg:w-1/2 flex justify-center">
+          <div className="relative w-full max-w-md aspect-square">
+            <Image
+              src={"/poster.png"}
+              alt="Scheduling ilustration"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-24">
+        <h2 className="text-3xl font-bold text-center mb-12 bg-gradient-to-b from-indigo-500 via-sky-400 to-cyan-300 bg-clip-text text-transparent">
+          Key Features
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          {features.map((feature, index) => {
+            return (
+              <Card key={index}>
+                <CardHeader>
+                  <feature.icon className="w-12 h-12 text-blue-500 mb-4 mx-auto" />
+                  <CardTitle className="text-center text-blue-600">
+                    {feature.title}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-center text-gray-600">
+                    {feature.description}
+                  </p>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="mb-24">
+        <h2 className="text-3xl font-bold text-center mb-12 bg-gradient-to-b from-indigo-500 via-sky-400 to-cyan-300 bg-clip-text text-transparent">
+          What Our Users say
+        </h2>
+        <TestimonialCarousel />
+      </div>
+
+      <div className="mb-24">
+        <h2 className="text-3xl font-bold text-center mb-12 bg-gradient-to-b from-indigo-500 via-sky-400 to-cyan-300 bg-clip-text text-transparent">
+          How it Works
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {howItWorks.map((step, index) => (
+            <div key={index} className="text-center">
+              <div className="bg-blue-100 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                <span className="text-blue-600 font-bold text-xl">
+                  {index + 1}
+                </span>
+              </div>
+              <h3 className="font-semibold text-lg mb-2">{step.step}</h3>
+              <h3 className="text-gray-600">{step.description}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="bg-blue-600 text-white rounded-lg p-8 text-center">
+        <h2 className="text-3xl font-bold mb-4">
+          Ready to Simplify Your Scheduling
+        </h2>
+        <p className="text-xl mb-6">
+          Join thousands of professionals who trust Schedulerrr for efficient
+          time
+        </p>
+
+        <Link href={"/dashboard"}>
+          <Button size={"lg"} variant={"secondary"} className={"text-blue-600"}>
+            Start for free <ArrowRight className="ml-2 h-5 w-5" />
+          </Button>
+        </Link>
+      </div>
+    </main>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { PenBox } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import UserMenu from "./user-menu";
 
 const Header = () => {
   return (
@@ -23,7 +25,16 @@ const Header = () => {
             Create Event
           </Button>
         </Link>
+
+        <SignedOut>
+        <SignInButton forceRedirectUrl="/dashboard">
         <Button variant={"outline"}>Login</Button>
+        </SignInButton>
+        </SignedOut>
+        
+        <SignedIn>
+          <UserMenu/>
+        </SignedIn>
       </div>
     </nav>
   );
